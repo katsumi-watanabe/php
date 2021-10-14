2d230f6a13f0bd2dd5c23f257640591ead248c56
katsumi-watanabe <ktm981124@gmail.com>
2021-10-14 19:20:08 +0900

    php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-echo('test');
+// echo('こんにちは');
+// echo('<br>');
+// echo("こんばんわ");
+// echo('<br>');
+// echo('こちらはPHPです');
+
+// 変数
+
+$test = 123;
+$test = 'テストです';
+
+echo $test;
 
 ?>
+
