a99743d836e128c780fb91a55855513cb6b9fb23
katsumi-watanabe <ktm981124@gmail.com>
2021-10-16 18:19:12 +0900

    php

--- array.php
+++ array.php
@@ -1,10 +1,18 @@
 <?php
 
-$array = ['bus',2,3];
+$array_1 = ['bass',2,3];
+
+$array_2 = [
+  ['赤','青','黄'],
+  ['緑','紫','黒']
+];
 // echo $array[1];
+// var_dump($array_1);
 
 echo '<pre>';
-var_dump($array);
+var_dump($array_2);
 echo '</pre>';
+
+echo $array_2[1][1];
 
 ?>
