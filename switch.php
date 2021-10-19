c27cee3a6861bc53e83b6dd920a5d868cd5f797b
katsumi-watanabe <ktm981124@gmail.com>
2021-10-19 22:55:48 +0900

    switch文

--- switch.php
+++ switch.php
@@ -5,6 +5,9 @@
 
 $data = 5;
 
+// ==　switch
+// === 型も等しい
+
 switch($data){
   case 1:
     echo '1です';
@@ -17,6 +20,36 @@ switch($data){
     break;
   default:
   echo '1-3ではありません';
+}
+
+
+echo '<br>';
+$data = '1';
+
+switch($data){
+  case $data === '1':
+    echo '1です';
+    break;
+  case 2:
+    echo '2です';
+    break;
+  case 3:
+    echo '3です';
+    break;
+  default:
+  echo '1-3ではありません';
+}
+
+if($data === 1){
+  echo '1';
+}
+
+if($data === 2){
+  echo '2';
+}
+
+if($data === 3){
+  echo '3';
 }
 
 ?>
