422f38adba257ca33b7a29e339d849b7d95174f1
katsumi-watanabe <ktm981124@gmail.com>
2021-10-19 22:44:27 +0900

    for文

--- for.php
+++ for.php
@@ -1,9 +1,24 @@
 <?php
 
-// for,while
+// for繰り返す数が決まっていたら
+// while繰り返す数が決まっていなかったら
 
 for($i = 0; $i < 10; $i++){
+  if($i === 5){
+    // break;
+    // continue;
+  }
   echo $i;
 }
 
+echo '<br>';
+
+$j = 0;
+while($j < 5){
+  echo $j;
+  $j++;
+}
+
+do{echo $j;}
+while($j < 5);
 ?>
