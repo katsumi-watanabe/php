0099db17142ebe5c4dbbeae916a0fc9f69156ae5
katsumi-watanabe <ktm981124@gmail.com>
2021-10-20 22:49:31 +0900

    関数

--- function.php
+++ function.php
@@ -6,4 +6,26 @@
 //   return // 戻り値;
 // }
 
+
+// インプット引数なし
+// アウトプット戻り値なし
+
+function test(){
+  echo 'テスト';
+}
+
+test();
+
+// インプット引数あり
+// アウトプット戻り値なし
+
+$comment = 'コメント2';
+$comment3 = 'コメント3';
+
+function getComment($string){
+  echo $string;
+}
+
+getComment($comment3);
+
 ?>
