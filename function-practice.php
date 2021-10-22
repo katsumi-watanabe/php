a401e464780972f2d94c80d3528e6e3d6008bb94
katsumi-watanabe <ktm981124@gmail.com>
2021-10-23 00:50:57 +0900

    function

---
+++ function-practice.php
@@ -0,0 +1,23 @@
+<?php
+
+$postalCode = '123-45678';
+
+//camelCase
+function checkPostalCode($str){
+  $replaced = str_replace('-','',$str);
+  $length = strlen($replaced);
+
+
+  var_dump($length);
+  if($length === 7){
+    return true;
+  }
+  return false;
+}
+
+var_dump(checkPostalCode($postalCode));
+
+//snakeCase
+// check_post_code()
+
+?>
