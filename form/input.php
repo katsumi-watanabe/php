9862e82d4fd9d22071bd1870f82e290eca75e2ad
katsumi-watanabe <ktm981124@gmail.com>
2021-10-25 23:39:59 +0900

    form

---
+++ form/input.php
@@ -0,0 +1,38 @@
+<?php
+
+if(!empty($_POST)){
+  echo '<pre>';
+  var_dump($_POST);
+  echo '</pre>';
+}
+// スーパーグローバル変数 php 9種類
+// 連想配列
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<form method="POST" action="input.php">
+氏名
+<input type="text" name="your_name">
+<br>
+<input type="checkbox" name="sport[]" value="野球">野球
+<input type="checkbox" name="sport[]" value="サッカー">サッカー
+<input type="checkbox" name="sport[]" value="バスケ">バスケ
+
+
+<input type="submit" value="送信">
+
+
+
+</form>
+
+</body>
+</html>
