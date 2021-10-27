30ac7aea50ed42e670aa806c620c0d5b714db32b
katsumi-watanabe <ktm981124@gmail.com>
2021-10-27 23:41:59 +0900

    フォームセキュリティー

--- form/input.php
+++ form/input.php
@@ -1,10 +1,15 @@
 <?php
 // スーパーグローバル変数 php 9種類
 // 連想配列
-if(!empty($_POST)){
-  echo '<pre>';
-  var_dump($_POST);
-  echo '</pre>';
+
+// if(!empty($_POST)){
+//   echo '<pre>';
+//   var_dump($_POST);
+//   echo '</pre>';
+// }
+
+function h($str){
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
 // 入力、確認、完了 input.php, confirm.php, thanks.php
@@ -37,10 +42,10 @@ if(!empty($_POST['btn_submit'])){
 <!-- 入力画面 -->
 <form method="POST" action="input.php">
 氏名
-<input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo $_POST['your_name'];}?>">
+<input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']);}?>">
 <br>
 メールアドレス
-<input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo $_POST['email'];}?>">
+<input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']);}?>">
 <br>
 <input type="submit" name="btn_confirm" value="確認する">
 </form>
@@ -50,15 +55,15 @@ if(!empty($_POST['btn_submit'])){
 <!-- 確認画面 -->
 <form method="POST" action="input.php">
 氏名
-<?php echo $_POST['your_name'];?>
+<?php echo h($_POST['your_name']);?>
 <br>
 メールアドレス
-<?php echo $_POST['email'];?>
+<?php echo h($_POST['email']);?>
 <br>
 <input type="submit" name="back" value="戻る">
 <input type="submit" name="btn_submit" value="送信する">
-<input type="hidden" name="your_name" value="<?php echo $_POST['your_name'];?>">
-<input type="hidden" name="email" value="<?php echo $_POST['email'];?>">
+<input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']);?>">
+<input type="hidden" name="email" value="<?php echo h($_POST['email']);?>">
 </form>
 <?php endif; ?>
 
