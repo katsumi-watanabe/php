b928bcf353ca048da538c30f82049d87cd706f9b
katsumi-watanabe <ktm981124@gmail.com>
2021-11-01 23:17:34 +0900

    validation

--- form/validation.php
+++ form/validation.php
@@ -4,8 +4,23 @@ function validation($request){ // $_POST連想配列
 
   $errors = [];
 
-  if(empty($request['your_name'])){
-    $errors[] = '氏名は必須です';
+  if(empty($request['your_name']) || 20 < mb_strlen($request['your_name'])){
+    $errors[] = '「氏名」は必須です。20文字以内で入力してください。';
+  }
+  if(empty($request['contact']) || 200 < mb_strlen($request['contact'])){
+    $errors[] = '「お問い合わせ内容」は必須です。200文字以内で入力してください。';
+  }
+
+  if(empty($request['caution'])){
+    $errors[] = '「注意事項」をご確認ください。';
+  }
+
+  if (!isset($request['gender'])){
+    $errors[] = '「性別」は必須です。'
+  }
+
+  if (empty($request['age']) || 6 < $request['age']){
+    $errors[] = '「年齢」は必須です。'
   }
 
   return $errors;
