9ce95662269d2539602ef099475a0d304922e6f7
katsumi-watanabe <ktm981124@gmail.com>
2021-11-02 23:17:10 +0900

    Bootstrap

--- form/input.php
+++ form/input.php
@@ -62,7 +62,7 @@ if(!empty($_POST['btn_submit'])){
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-  
+
 
 <?php if($pageFlag === 0) : ?>
 <?php
@@ -84,10 +84,19 @@ $token = $_SESSION['csrfToken'];
   <?php echo '</ul>' ;?>
 <?php endif ;?>
 <!-- 入力画面 -->
-<form method="POST" action="input.php">
-氏名
-<input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']);}?>">
-<br>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <form method="POST" action="input.php">
+      <div class="form-group">
+        <label for="your_name">氏名</label>
+        <input type="text" class="form-control" id="your_name" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']);}?>" require>
+      </div>
+    </div>
+  </div>
+</div>
+
 メールアドレス
 <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']);}?>">
 <br>
