1c0fbe083d7e5ab0a3d6445254a9b4e62501a40d
katsumi-watanabe <ktm981124@gmail.com>
2021-11-04 00:04:17 +0900

    隠しファイル

--- mainte/test.php
+++ mainte/test.php
@@ -2,10 +2,10 @@
 // パスワードを記録したファイルの場所
 
 echo __FILE__;
-// /Applications/MAMP/htdocs/php_test/form/test.php
+// /Applications/MAMP/htdocs/php_test/mainte/test.php
 
 echo '<br>';
 // パスワード(暗号化)
 echo(password_hash('password123', PASSWORD_BCRYPT));
-// $2y$10$ypxmCiV0LexqMG8VEdH18u6uRaKScaf8IZnzUENMlewq9opjYgyBC
+// $2y$10$56G3G33MDYeeuKK9R/YX3OnhFC1JYiD50OlOLL1pK.fpxd2u5sei2
 ?>
