e0041557ecbd4c66f0af28c6d5c8e6161b144c32
katsumi-watanabe <ktm981124@gmail.com>
2021-11-03 23:38:21 +0900

    隠しファイル

--- form/test.php
+++ form/test.php
@@ -4,6 +4,8 @@
 echo __FILE__;
 // /Applications/MAMP/htdocs/php_test/form/test.php
 
+echo '<br>';
 // パスワード(暗号化)
-echo(password_hash)
+echo(password_hash('password123', PASSWORD_BCRYPT));
+// $2y$10$ypxmCiV0LexqMG8VEdH18u6uRaKScaf8IZnzUENMlewq9opjYgyBC
 ?>
