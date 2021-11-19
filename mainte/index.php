b7450c673124b69493d9f934dabd4ddc422faa88
katsumi-watanabe <ktm981124@gmail.com>
2021-11-19 21:55:06 +0900

    rollback,Transaction

--- mainte/index.php
+++ mainte/index.php
@@ -14,12 +14,28 @@ require 'db_connection.php';
 
 // ユーザー入力あり prepare, bind, execute 悪意ユーザ delete * SQLインジェクション
 $sql = 'select * from contacts where id = :id'; // 名前付きプレースホルダー
-$stmt = $pdo->prepare($sql);// プリペアードステートメント
-$stmt->bindValue('id', 2, PDO::PARAM_INT);// 紐付け
-$stmt->execute(); // 実行
 
 $result = $stmt->fetchall();
 
 echo '<pre>';
 var_dump($result);
 echo '</pre>';
+
+// トランザクション まとまって処理 beginTransaction, commit, rollback
+// ex)銀行 残高を確認->Aさんから引き落とし->Bさんに振込
+
+$pdo->beginTransaction();
+
+try{
+
+// sql処理
+$stmt = $pdo->prepare($sql);// プリペアードステートメント
+$stmt->bindValue('id', 2, PDO::PARAM_INT);// 紐付け
+$stmt->execute(); // 実行
+
+$pdo->commit();
+
+}catch(PDOException $e){
+
+  $pdo->rollback();// 更新のキャンセル
+}
