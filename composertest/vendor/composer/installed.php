48700cf54ad0f666b1c4fffa9bba8832445d8625
katsumi-watanabe <ktm981124@gmail.com>
2022-02-05 17:34:06 +0900

    composer

---
+++ composertest/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5487ade46b390094b9996fae0a425b46c19ba144',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5487ade46b390094b9996fae0a425b46c19ba144',
+            'dev_requirement' => false,
+        ),
+    ),
+);
